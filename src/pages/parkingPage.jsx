2386beb147d5a6bcb28765a4a6f53a5ea27baa37
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-10-13 19:54:31 +0200

    Fix file names and imports

--- src/pages/parkingPage.jsx
+++ src/pages/parkingPage.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { getParking } from "../actions/parkingActions";
-import ParkingDetail from "../components/ParkingDetail";
+import ParkingDetail from "../components/parkingDetail";
 
 class ParkingPage extends Component {
   constructor(props) {
