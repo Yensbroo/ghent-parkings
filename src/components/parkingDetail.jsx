860ad85245e051452863d19c75a7f7fc7c904750
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-10-14 14:44:32 +0200

    Change parkingDetail layout

--- src/components/parkingDetail.jsx
+++ src/components/parkingDetail.jsx
@@ -17,56 +17,54 @@ const ParkingDetail = ({ parking }) => {
             <img className="img-fluid" src={Images[parking.id]} alt="" />
           </div>
           <div className="col-lg-6">
-            <div className="row">
-              <div className="col-lg-6 mb-3">
-                <div className="parkingItem">
-                  <p>
-                    <strong>Address:</strong> <br />
-                    {parking.address}
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-6 mb-3">
-                <div className="parkingItem">
-                  <p>
-                    <strong>Contact:</strong> <br />
-                    {parking.contactInfo}
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-6 mb-3">
-                <div className="parkingItem">
-                  <p>
-                    <strong>
-                      Status: <br />
-                    </strong>
-                    {parking.parkingStatus.open ? "Open" : "Closed"}
-                  </p>
-                </div>
-              </div>
-              <div className="col-lg-6 mb-3">
-                <div className="parkingItem">
-                  <p>
-                    <strong>Capacity: </strong> <br />
-                    {parking.parkingStatus.availableCapacity}/{parking.parkingStatus.totalCapacity}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <h3 className="col-lg-12 mb-3">Opening hours</h3>
-            <div className="col-lg-12 d-flex mb-3">
+            <div className="parkingHours">
+              <h4 className="mb-3">Opening hours</h4>
               {parking.openingTimes.map(item => {
                 return item.days.map(day => {
                   return (
-                    <p key={day} className="day">
-                      <strong>{day}:</strong> <br />
-                      from {item.from} <br /> to {item.to}
+                    <p>
+                      <strong>{day}: </strong>
+                      {item.from} to {item.to}
                     </p>
                   );
                 });
               })}
+            </div>
+          </div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-lg-3 mb-3">
+            <div className="parkingItem">
+              <p>
+                <strong>Address:</strong> <br />
+                {parking.address}
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-3 mb-3">
+            <div className="parkingItem">
+              <p>
+                <strong>Contact:</strong> <br />
+                {parking.contactInfo}
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-3 mb-3">
+            <div className="parkingItem">
+              <p>
+                <strong>
+                  Status: <br />
+                </strong>
+                {parking.parkingStatus.open ? "Open" : "Closed"}
+              </p>
+            </div>
+          </div>
+          <div className="col-lg-3 mb-3">
+            <div className="parkingItem">
+              <p>
+                <strong>Capacity: </strong> <br />
+                {parking.parkingStatus.availableCapacity}/{parking.parkingStatus.totalCapacity}
+              </p>
             </div>
           </div>
         </div>
